847422fa44be7098ee23c2efdd568fcee0e93be5
Mackenzie <mackenziekynoch@gmail.com>
2021-05-27 20:24:38 -0400

    basic setup for service

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h2>Hello World</h2>
+    );
+  }
+};
+
+export default App;
