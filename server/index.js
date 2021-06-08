38d609fe979e0e0d95c62c97626f2518b20e77bd
Mackenzie <mackenziekynoch@gmail.com>
2021-06-07 22:45:54 -0600

    added cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const cors = require('cors');
 
 const port = process.env.PORT || 4000;
 
 const { findRelated } = require('../database/methods/query.js');
 
 app.use(express.static(path.join(__dirname, '..', '/public')));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.end();
