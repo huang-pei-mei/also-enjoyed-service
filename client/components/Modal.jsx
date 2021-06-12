591d0cd98d3230f8ac21a39fb74e4a8ce593e923
Mackenzie <mackenziekynoch@gmail.com>
2021-06-12 00:15:39 -0600

    Added review service to modal data

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -14,16 +14,19 @@ class Modal extends React.Component {
 
   componentDidMount() {
     const bookId = this.props.book.id;
-    const reviewsService = 'http://54.183.2.218:4001/reviews/carouselReviews';
+    // const reviewsService = 'http://54.183.2.218:4001/reviews/carouselReviews';
+    const reviewsService = 'http://localhost:4001/reviews/carouselReviews';
     const summaryService = `http://ec2-18-188-135-5.us-east-2.compute.amazonaws.com:1220/api/summaries/${bookId}`;
     const aggReviewService = `http://ec2-18-220-21-137.us-east-2.compute.amazonaws.com:2880/api/aggReview/${bookId}`;
 
-    requests.get(reviewsService)
+    requests.post(reviewsService, {
+      ids: [bookId]
+    })
       .then((data) => {
         console.log('review data: ', data);
-        // return this.setState({
-        //   review: data
-        // })
+        return this.setState({
+          review: data.data[0]
+        })
       });
     requests.get(summaryService)
       .then((data) => {
@@ -39,6 +42,11 @@ class Modal extends React.Component {
           aggReviews: data.data[0]
         })
       });
+  }
+
+  formatDate(date) {
+    let inputDate = new Date(date);
+    return `${String(inputDate.getMonth() + 1).padStart(2,'0')}-${String(inputDate.getDate()).padStart(2,'0')}-${String(inputDate.getYear()).substr(0,2)}`;
   }
 
   render() {
@@ -74,11 +82,13 @@ class Modal extends React.Component {
           <p className='also-enjoyed-modal-summary'>{this.state.summary.short_summary}</p>
         }
         <hr />
-        <div className='also-enjoyed-modal-review'>
-          <span className='also-enjoyed-modal-stars'></span>
-          <h4 className='also-enjoyed-modal-review-title'>"Review Title"</h4>
-          <p className='also-enjoyed-modal-review-author'>By <em>Name</em> on mm-dd-yy</p>
-        </div>
+        {this.state.review &&
+          <div className='also-enjoyed-modal-review'>
+            <span className='also-enjoyed-modal-stars'></span>
+            <h4 className='also-enjoyed-modal-review-title'>{`"${this.state.review.reviewTitle}"`}</h4>
+            <p className='also-enjoyed-modal-review-author'>By <em>{this.state.review.reviewerName}</em> on {this.formatDate(this.state.review.date)}</p>
+          </div>
+        }
       </div>
     );
   }
